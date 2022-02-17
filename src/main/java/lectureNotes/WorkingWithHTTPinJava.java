7ab8fafb87514f1503eda356c79c2e1d349e9f08
SergeyHodak <4Kserge4Kserge4K@gmail.com>
2022-02-17 17:40:19 +0200

    конспект 2 из 3

--- src/main/java/lectureNotes/WorkingWithHTTPinJava.java
+++ src/main/java/lectureNotes/WorkingWithHTTPinJava.java
@@ -33,62 +33,112 @@ public class WorkingWithHTTPinJava {}
 
 
 class HelloWorld {
+    private static final String TEST_URL = "https://jsonplaceholder.typicode.com/users"; // текстовое поле (в начале протокол "https:...")
 
-    private static final String TEST_URL =
-        "https://jsonplaceholder.typicode.com/users";
-
-    public static void main(String[] args) throws IOException {
-        sendGET();
+    public static void main(String[] args) throws IOException { // запускалка с исключением
+        sendGET(); // метод запроса о получении информации со ссылки
         sendPOST();
     }
 
-    private static void sendGET() throws IOException {
-        URL url = new URL(TEST_URL);
+    private static void sendGET() throws IOException { // Метод - отправитьПОЛУЧИТЬ. + исключение (запрос о Получении)
+        URL url = new URL(TEST_URL); // новый экземпляр класса URL, со ссылкой из текстового поля
+
+        // После получения сущности URL, вызываем метод openConnection() который возвратит нам сущность HttpsUrlConnection.
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-        System.out.println("GET response code: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) {
+        connection.setRequestMethod("GET"); // для выполнения HTTP GET
+
+        int responseCode = connection.getResponseCode(); // узнать HTTP статус
+        System.out.println("GET response code: " + responseCode); // посмотреть этот статус
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // если равно 200
             BufferedReader in =
-                new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
+                new BufferedReader( // Java класс BufferedReader читает текст из потока ввода символов,
+                                    // буферизуя прочитанные символы, чтобы обеспечить эффективное
+                                    // считывание символов, массивов и строк.
+                    new InputStreamReader( // InputStreamReader преобразует InputStream к Reader
+                            connection
+                                    .getInputStream() // Возвращает входной поток, который считывается из
+                                                      // этого открытого соединения.
+                    ));
+
+            String inputLine; // строковая переменная
+            StringBuffer response = new StringBuffer(); // Поточно-безопасная изменяемая последовательность символов.
+                                                        // Строковый буфер подобен String, но может быть изменен. В
+                                                        // любой момент времени он содержит определенную
+                                                        // последовательность символов, но длину и содержимое
+                                                        // последовательности можно изменить с помощью вызовов
+                                                        // определенных методов.
             while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                // readLine() - Читает строку текста. Строка считается завершенной одним из следующих символов:
+                // перевод строки ('\n'), возврат каретки ('\r'), возврат каретки, за которым сразу следует перевод
+                // строки, или достижение конца файла. (ЭОФ).
+                // Возвращает: Строка, содержащая содержимое строки, не включая символы завершения строки, или null,
+                // если конец потока был достигнут без чтения каких-либо символов.
+                response.append(inputLine); // добавить на вывод
             }
-            in.close();
-            System.out.println(response.toString());
-        } else {
-            System.out.println("GET request not worked");
+            in.close(); // завершить поток
+            System.out.println(response.toString()); // печатать результат в консоль
+        } else { // иначе, не равно 200
+            System.out.println("GET request not worked"); // сообщение: GET-запрос не работает
         }
     }
 
-    private static void sendPOST() throws IOException {
-        URL url = new URL(TEST_URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        OutputStream os = connection.getOutputStream();
-        os.write(Files.readAllBytes(new File("src\\main\\java\\lectureNotes\\user.json").toPath()));
-        os.flush();
-        os.close();
+    private static void sendPOST() throws IOException { // отправить сообщение + исключение
+        URL url = new URL(TEST_URL); // новый экземпляр класса URL, со ссылкой из текстового поля
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // После получения сущности URL,
+                                                                                 // вызываем метод openConnection()
+                                                                                 // который возвратит нам сущность
+                                                                                 // HttpsUrlConnection.
+        connection.setRequestMethod("POST"); // для выполнения HTTP POST
+        connection.setDoOutput(true); // Устанавливает значение поля doOutput для этого URLConnection в указанное
+                                      // значение. Соединение URL может использоваться для ввода и/или вывода.
+                                      // Установите для флага doOutput значение true, если вы собираетесь использовать
+                                      // URL-соединение для вывода, и значение false, если нет. Значение по
+                                      // умолчанию — ложь. Параметры: dooutput – новое значение.
+        OutputStream os = connection.getOutputStream(); // getOutputStream() - Возвращает выходной поток, который
+                                                        // записывает в это соединение.
+        os.write( // Записывает байты b.length из указанного массива байтов в этот выходной поток.
+                Files   // Этот класс состоит исключительно из статических методов, которые работают с файлами,
+                        // каталогами или другими типами файлов.
+                        .readAllBytes( // Читает все байты из файла.
+                                new File( // Создает новый экземпляр File путем преобразования заданной строки пути
+                                          // в абстрактный путь.
+                                        "src\\main\\java\\lectureNotes\\user.json")
+                                        .toPath())); // Возвращает объект java.nio.file.Path, созданный из этого
+                                                     // абстрактного пути. Полученный путь связан с файловой
+                                                     // системой по умолчанию.
+        os.flush(); // Сбрасывает этот выходной поток и принудительно записывает любые буферизованные выходные байты.
+        os.close(); // Закрывает этот выходной поток и освобождает все системные ресурсы, связанные с этим потоком.
 
-        int responseCode = connection.getResponseCode();
-        System.out.println("POST response code: " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_CREATED) {
+        int responseCode = connection.getResponseCode(); // узнать HTTP статус
+        System.out.println("POST response code: " + responseCode); // посмотреть этот статус
+        if (responseCode == HttpURLConnection.HTTP_CREATED) { // если равно 201
             BufferedReader in =
-                new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            StringBuffer response = new StringBuffer();
-            String inputLine;
+                new BufferedReader( // Java класс BufferedReader читает текст из потока ввода символов,
+                                    // буферизуя прочитанные символы, чтобы обеспечить эффективное
+                                    // считывание символов, массивов и строк.
+                    new InputStreamReader( // InputStreamReader преобразует InputStream к Reader
+                            connection.getInputStream())); // Возвращает входной поток, который считывается из
+                                                           // этого открытого соединения.
+            StringBuffer response = new StringBuffer(); // Поточно-безопасная изменяемая последовательность символов.
+                                                        // Строковый буфер подобен String, но может быть изменен. В
+                                                        // любой момент времени он содержит определенную
+                                                        // последовательность символов, но длину и содержимое
+                                                        // последовательности можно изменить с помощью вызовов
+                                                        // определенных методов.
+            String inputLine; // строковая переменная
             while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                // readLine() - Читает строку текста. Строка считается завершенной одним из следующих символов:
+                // перевод строки ('\n'), возврат каретки ('\r'), возврат каретки, за которым сразу следует перевод
+                // строки, или достижение конца файла. (ЭОФ).
+                // Возвращает: Строка, содержащая содержимое строки, не включая символы завершения строки, или null,
+                // если конец потока был достигнут без чтения каких-либо символов.
+                response.append(inputLine); // добавить на вывод
             }
-            in.close();
-            System.out.println(response.toString());
-        } else {
-            System.out.println("POST request not worked");
+            in.close(); // завершить поток
+            System.out.println(response.toString()); // печатать результат в консоль
+        } else { // иначе, не равно 201
+            System.out.println("POST request not worked"); // сообщение: POST-запрос не работает
         }
     }
 }
